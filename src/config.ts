da8b661a11e3dcad7bdce3e4cdc10048b74a9aef
David G. Moore, Jr <david@dgmjr.io>
2024-06-23 16:15:29 -0400

    Changing title, subtitle, & tagline

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
-export const SUBTITLE = "A free clone of LinkTree"
-export const TITLE = "LinkTreeFreeClone"
-export const TAGLINE = "Make your own LinkTree page for free"
+export const SUBTITLE = "Washington, DC-based software engineer and college professor"
+export const TITLE = "Dr. David G. Moore, Jr."
+export const TAGLINE = "<i>In the end, it's not the years in your life that matter, but the life in your years</i><br/>- A. Lincoln"
 
 // Contacts
-export const contact = "Keep in touch"
-export const phone = "+33600000000"
-export const email = "contact@yoandev.co"
+export const contact = "Contact me"
+export const phone = "+14102125869"
+export const email = "david@dgmjr.io"
